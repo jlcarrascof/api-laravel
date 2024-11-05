e949bafbf1772c1ec740dbb2f842288a692bed96
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-05 02:26:09 -0400

    Remove RouteServiceProvider and update routing configuration for Sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 
-Route::apiResource('products', ProductController::class);
-
-Route::get('/test', function () {
-    return response()->json(['message' => 'API is working!']);
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
