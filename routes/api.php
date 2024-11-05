38c8040f6c9ca375af9d47a78446fcfe4cbe0ebe
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-04 23:08:10 -0400

    Add test route to verify API functionality

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::apiResource('products', ProductController::class);
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working!']);
+});
