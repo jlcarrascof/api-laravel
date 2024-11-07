b98cad331bea4e62fd07629164b51865311668c9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-07 12:13:53 -0400

    Add custom validation messages for product creation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -35,6 +35,14 @@ class ProductController extends Controller
             'price' => 'required|numeric|min:0',
             'stock' => 'required|integer|min:0',
             'category_id' => 'nullable|integer|exists:categories,id'
+        ], [
+            'name.required' => 'Product name is mandatory.',
+            'name.max' => 'Product name only support until 255 characters.',
+            'price.required' => 'Price is mandatory and must be a positive number.',
+            'price.min' => 'Price cant be a negative number.',
+            'stock.required' => 'Stock is mandatory and must be a positive number.',
+            'stock.min' => 'Stock cant be a negative number.',
+            'category_id.exists' => 'Category selected doesnt exists.'
         ]);
 
         $product = Product::create($validated);
