8cf40b43c7961ef9fb33c4d7d7f1a0f935d14288
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-06 23:36:52 -0400

    Implement product deletion in destroy method and return JSON response

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -78,8 +78,9 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully']);
     }
 }
