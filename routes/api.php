ed5e72bbbd8b02a6ba17d4eea51e1b2df5fc1499
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-08 19:22:07 -0400

    Add CategoryController import to API routes

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 Route::apiResource('products', ProductController::class);
