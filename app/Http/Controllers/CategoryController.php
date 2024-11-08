94596d747e72973a3706e0f2544b162e64782e9e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-08 19:21:09 -0400

    Add showProducts method to CategoryController for retrieving products by category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,5 +7,14 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    
+    public function showProducts($id)
+    {
+        $category = Category::with('products')->find($id);
+
+        if (!$category) {
+            return response()->json(['message' => 'Categoría no encontrada'], 404);
+        }
+
+        return response()->json($category);
+    }
 }
