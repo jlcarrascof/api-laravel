61c2931fa5885f4cbfc37a2e04a8242abc8717ad
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-15 11:05:02 -0400

    Enhance ProductResource to include detailed product attributes and category information

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\CategoryResource;
 
 class ProductResource extends JsonResource
 {
@@ -14,6 +15,13 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'stock' => $this->stock,
+            'category' => new CategoryResource($this->whenLoaded('category')),
+        ];
     }
 }
