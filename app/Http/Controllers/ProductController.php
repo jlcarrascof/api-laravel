53f065e20ce0186510b72a67032feabcf6961f05
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-23 11:19:08 -0400

    Return product data in a structured format in the show method of ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -80,7 +80,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        return response()->json($product);
+        return response()->json(['data' => $product], 200);
     }
 
     /**
